caf372230a399704306f88788bd018bc3ae044c7
WatcharachaiSk <watcharachai.sa@rmuti.ac.th>
2023-03-30 15:36:09 +0700

    use V-on
    use V-on

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import VueSidebarMenu from "vue-sidebar-menu";
+import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(VueSidebarMenu);
+app.mount("#app");
